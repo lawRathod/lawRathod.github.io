50977083da300bbf9d8b64cdea7cb5e4efa53b4b
Prateek <emaillawrathod@gmail.com>
2020-08-16 17:58:47 +0530

    test build

---
+++ precache-manifest.7c01b5aee307479b35f4eeae5a896048.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b115f661725b4b92b56e599350dca15f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44604cb4316832883d4d",
+    "url": "/static/css/main.5aebb216.chunk.css"
+  },
+  {
+    "revision": "3cdd75864d4ff372e1a9",
+    "url": "/static/js/2.8e94ff9a.chunk.js"
+  },
+  {
+    "revision": "544111c69119227bbf075a65771ccdb1",
+    "url": "/static/js/2.8e94ff9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44604cb4316832883d4d",
+    "url": "/static/js/main.ab48055e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a1acb00f5bffd4e7b86f0cfb89fa184a",
+    "url": "/static/media/FiraSans-Bold.a1acb00f.ttf"
+  },
+  {
+    "revision": "d7fafde2fe8dd421fc8db1a7f48965d6",
+    "url": "/static/media/FiraSans-Medium.d7fafde2.ttf"
+  },
+  {
+    "revision": "895f5b025a6cc4924b263f6beb06c777",
+    "url": "/static/media/FiraSans-Regular.895f5b02.ttf"
+  }
+]);
